e10c19fcb589a7036a673c60734b17b979e482b7
rifkifauzi-versia <rifkifauzi.versia@gmail.com>
2023-06-14 20:57:41 +0700

    LL

--- bin/dart_mkpl.dart
+++ bin/dart_mkpl.dart
@@ -1,5 +1,5 @@
 import 'package:dart_mkpl/dart_mkpl.dart' as dart_mkpl;
 
 void main(List<String> arguments) {
-  print('Hell world: ${dart_mkpl.calculate()}!');
+  print('Hel world: ${dart_mkpl.calculate()}!');
 }
